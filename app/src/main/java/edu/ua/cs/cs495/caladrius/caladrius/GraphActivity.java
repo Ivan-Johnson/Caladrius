698b60e13350a1f59132e51c7bbd7cd8d8a76fe3
Ian <ianbway@gmail.com>
2018-09-23 16:31:49 -0500

    Graph basic template

--- app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/GraphActivity.java
+++ app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/GraphActivity.java
@@ -102,10 +102,6 @@ public class GraphActivity extends AppCompatActivity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
