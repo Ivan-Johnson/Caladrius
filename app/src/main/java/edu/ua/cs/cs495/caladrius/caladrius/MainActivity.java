e1f97d650976a20fcf48c71cc0242ceb3daef83a
Ivan Johnson <git@IvanJohnson.net>
2018-09-23 05:05:13 -0500

    Add menu for code demo purposes

--- app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainActivity.java
+++ app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainActivity.java
@@ -2,6 +2,11 @@ package edu.ua.cs.cs495.caladrius.caladrius;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.foo, menu);
+        return true; // i.e, yes, we have a menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String text;
+        switch (item.getItemId()) {
+            case R.id.battery:
+                text = "battery";
+                break;
+            case R.id.delete:
+                text = "delete";
+                break;
+            case R.id.add:
+                text = "add";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        Log.i("sometag", "You clicked " + text);
+        return true; // i.e. yes, we used the event
     }
 }
