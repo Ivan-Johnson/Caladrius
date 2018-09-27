2fa68c1f51fc4eec92f8a46f06995cf3e497e448
Ivan Johnson <git@IvanJohnson.net>
2018-09-27 14:24:44 -0400

    Revert "Added GraphView Capability"
    This reverts commit 487604e889255a274d4409b9292c63d9c0e15231.

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainFragment.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainFragment.java
@@ -6,36 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
 public class MainFragment extends Fragment {
 
     public MainFragment() {
         // Empty public constructor
-
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.graph_list, container, false);
 
-        GraphView graph = rootView.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
-
         return rootView;
     }
-
-
-
 }
