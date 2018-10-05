46660d3708c8fc84fa2d2f34ea4665e1dce926c1
Hansheng Li <hli68@crimson.ua.edu>
2018-10-05 17:16:45 -0500

    Add new jave and xml file for calender and edit page:

---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/Edit.java
@@ -0,0 +1,15 @@
+package edu.ua.cs.cs495.caladrius.caladrius;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Edit extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit);
+
+    }
+}
