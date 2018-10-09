1d5957d427f21d6160cc06d377a0d249f1f0f5e1
Hansheng Li <hli68@crimson.ua.edu>
2018-10-08 23:07:53 -0500

    Add a calender in calender page

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/Calender.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/Calender.java
@@ -1,15 +1,32 @@
 package edu.ua.cs.cs495.caladrius.caladrius;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 
 public class Calender extends AppCompatActivity{
+
+    private static final String TAG = "CalenderActivity";
+
+    private CalendarView mCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calender);
+        mCalendarView = findViewById(R.id.calendarView);
 
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int i1, int i2) {
+                Integer month = i1 + 1;
+                String date = year + "/" + month + "/" + i2;
+//                Log.d(TAG, "onSelectedDayChange: date" + date);
+                Toast.makeText(mCalendarView.getContext(), "You pressed on : " + date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
