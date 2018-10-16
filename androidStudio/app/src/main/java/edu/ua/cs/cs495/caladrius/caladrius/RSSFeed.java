d6a260f2c10fbf02d779585f3eede1dad10d0c70
Ivan Johnson <git@IvanJohnson.net>
2018-10-16 12:19:55 -0500

    Dynamically generate feed list

---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RSSFeed.java
@@ -0,0 +1,9 @@
+package edu.ua.cs.cs495.caladrius.caladrius;
+
+public class RSSFeed {
+    String url;
+
+    public RSSFeed(String url) {
+        this.url = url;
+    }
+}
--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RSSFeedList.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RSSFeedList.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class RSSFeedList extends Fragment {
 
@@ -15,6 +16,15 @@ public class RSSFeedList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.rss_feed_list, container, false);
+
+        ListView ll = rootView.findViewById(R.id.FeedList);
+
+        final int len = 20;
+        RSSFeed[] feeds = new RSSFeed[len];
+        for (int c = 0; c < len; c++) {
+            feeds[c] = new RSSFeed("URL - " + Integer.toString(c));
+        }
+        ll.setAdapter(new RSSFeedItemAdapter(this.getContext(), feeds));
 
         return rootView;
     }
