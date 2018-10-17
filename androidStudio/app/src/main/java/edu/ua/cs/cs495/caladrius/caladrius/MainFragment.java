e696e7715156b9b36a9d4acca92f27e42526672b
Ian <ianbway@gmail.com>
2018-10-16 19:46:46 -0500

    Fixed GraphActivity. Made GraphPngs GraphView Objects

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainFragment.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/MainFragment.java
@@ -1,6 +1,7 @@
 package edu.ua.cs.cs495.caladrius.caladrius;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,8 +10,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.GridLabelRenderer;
 
 public class MainFragment extends Fragment {
     public MainFragment() {
@@ -32,6 +36,55 @@ public class MainFragment extends Fragment {
                 new DataPoint(4, 6)
         });
         graph.addSeries(series);
+
+        GraphView graph2 = rootView.findViewById(R.id.graph2);
+        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, -1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph2.addSeries(series2);
+
+        GraphView graph3 = rootView.findViewById(R.id.graph3);
+        PointsGraphSeries<DataPoint> series3 = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, -2),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph3.addSeries(series3);
+
+        GraphView graph4 = rootView.findViewById(R.id.graph4);
+        LineGraphSeries<DataPoint> series4 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 0),
+                new DataPoint(1, 1),
+                new DataPoint(2, 2),
+                new DataPoint(3, 3),
+                new DataPoint(4, 4)
+        });
+        graph4.addSeries(series4);
+
+        GraphView graph5 = rootView.findViewById(R.id.graph5);
+        LineGraphSeries<DataPoint> series5 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(2, 2),
+                new DataPoint(2, 2),
+                new DataPoint(4, 4),
+                new DataPoint(5, 5),
+                new DataPoint(6, 6)
+        });
+        graph5.addSeries(series5);
+        LineGraphSeries<DataPoint> series6 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(1, 5),
+                new DataPoint(2, 4),
+                new DataPoint(3, 3),
+                new DataPoint(4, 2),
+                new DataPoint(5, 1)
+        });
+        series6.setColor(Color.GREEN);
+        graph5.addSeries(series6);
 
         return rootView;
     }
