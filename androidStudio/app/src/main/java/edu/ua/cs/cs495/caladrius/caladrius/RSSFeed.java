124136fd2cb2d219b0e8eda8f695ec4b11770376
Ivan Johnson <git@IvanJohnson.net>
2018-10-18 08:54:51 -0500

    Add RssConditions to RssFeeds

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RSSFeed.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RSSFeed.java
@@ -1,9 +1,32 @@
 package edu.ua.cs.cs495.caladrius.caladrius;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RSSFeed {
     String url;
+    ArrayList<RssCondition> conditions;
 
-    public RSSFeed(String url) {
+    public RSSFeed(String url, ArrayList<RssCondition> conditions)
+    {
+        if (conditions == null) {
+            conditions = new ArrayList<>();
+        }
+
+        this.conditions = conditions;
         this.url = url;
+    }
+
+    public RSSFeed(String url, RssCondition[] conditions)
+    {
+        this(
+                url,
+                new ArrayList<>(Arrays.asList(conditions))
+        );
+    }
+
+    public RSSFeed(String url)
+    {
+        this(url, new ArrayList<RssCondition>());
     }
 }
---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/RssCondition.java
@@ -0,0 +1,8 @@
+package edu.ua.cs.cs495.caladrius.caladrius;
+
+import java.io.Serializable;
+
+public abstract class RssCondition implements Serializable
+{
+
+}
