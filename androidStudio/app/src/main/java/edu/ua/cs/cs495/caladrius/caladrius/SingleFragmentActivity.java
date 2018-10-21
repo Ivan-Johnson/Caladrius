6b1641fd8e638623edf895debe1c1720169ede07
Ivan Johnson <git@IvanJohnson.net>
2018-10-21 08:19:46 -0500

    Add class that wraps fragments in an activity

---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/caladrius/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package edu.ua.cs.cs495.caladrius.caladrius;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment makeFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.container);
+
+        if (fragment == null) {
+            fragment = makeFragment();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(R.id.container, fragment);
+            ft.commit();
+        }
+    }
+}
