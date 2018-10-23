3f0f966319b494b6d8b9dac251ee4224ed85dede
Ivan Johnson <git@IvanJohnson.net>
2018-10-23 15:59:16 -0500

    Define server-code build process

---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/server/Serverside.java
@@ -0,0 +1,11 @@
+package edu.ua.cs.cs495.caladrius.server;
+
+import java.io.SyncFailedException;
+
+public class Serverside
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!");
+	}
+}
