38c34f64ff98f2ef8ba68a84e8b82a34cf8a5560
Ivan Johnson <git@IvanJohnson.net>
2018-11-21 16:51:42 -0600

    Construct entire summary page from a list view adapter
    As there is no practical way to have a scroll view contain a scrollable
    list view, I moved the alldata button to be stored within the list view.
    This makes the adapter responsible for the entirety of the summary page,
    so I renamed it appropriately.

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/SummaryPage.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/SummaryPage.java
@@ -1,17 +1,23 @@
 package edu.ua.cs.cs495.caladrius.android;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+import org.json.JSONException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 /**
  * The SummaryPage module represents the main View that is exposed upon logging into the application.
@@ -49,6 +55,8 @@ public class SummaryPage extends Fragment
 		"Minutes of Activity",
 	};
 
+	Button alldata;
+
 	public SummaryPage()
 	{
 		// Empty public constructor
@@ -59,8 +67,6 @@ public class SummaryPage extends Fragment
 	                         Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		View rootView = inflater.inflate(R.layout.query_activity, container, false);
-
 
 		ArrayList queries = new ArrayList();
 
@@ -78,42 +84,42 @@ public class SummaryPage extends Fragment
 			queries.add(new Query(graphTypes, stats, color, title));
 		}
 
-		ListView ll = rootView.findViewById(R.id.graph_list);
+		alldata = new Button(getContext());
+		alldata.setText(R.string.view_all_data);
+		alldata.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
+
+		SummaryPageAdapter.FitbitGraphViewOnClickAdapter onClick = new SummaryPageAdapter.FitbitGraphViewOnClickAdapter() {
+			@Override
+			public void onClick(FitbitGraphView fgv) {
+				Intent submitPage = new Intent(getContext(), QueryActivity.class);
+				submitPage.putExtra("startDate", "N/A");
+				submitPage.putExtra("endDate", "N/A");
+				submitPage.putExtra("item_1", "N/A");
+				submitPage.putExtra("item_2", "N/A");
+				Objects.requireNonNull(getContext()).startActivity(submitPage);
+			}
+		};
+
+		ListView ll = new ListView(getContext());
 		try {
-			ll.setAdapter(new FitbitGraphViewAdapter(ll.getContext(), queries, new FitbitGraphViewAdapter.FitbitGraphViewOnClickAdapter() {
-				@Override
-				public void onClick(FitbitGraphView fgv) {
-					Intent submitPage = new Intent(getContext(), QueryActivity.class);
-					submitPage.putExtra("startDate", "N/A");
-					submitPage.putExtra("endDate", "N/A");
-					submitPage.putExtra("item_1", "N/A");
-					submitPage.putExtra("item_2", "N/A");
-					Objects.requireNonNull(getContext()).startActivity(submitPage);
-				}
-			}));
-		} catch (Exception e) {
+			ll.setAdapter(new SummaryPageAdapter(ll.getContext(), queries, onClick, alldata));
+		} catch (JSONException | InterruptedException | IOException | ExecutionException e) {
 			TextView tv = new TextView(ll.getContext());
-			tv.setText("Something bad happened.");
-			LinearLayout linearLayout = rootView.findViewById(R.id.topList);
-			linearLayout.addView(tv);
+			tv.setText("Caladrius failed to construct the summary page adapter; "+e.getLocalizedMessage());
+			ll.addView(tv);
 		}
 
-		return rootView;
+		return ll;
 	}
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState)
-	{
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-
-		final Button viewAllDataButton = getView().findViewById(R.id.viewAllData);
-		viewAllDataButton.setOnClickListener(new View.OnClickListener()
-		{
+		alldata.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
-				Intent nextScreen = new Intent(v.getContext(), AllData.class);
-				startActivityForResult(nextScreen, 0);
+			public void onClick(View view) {
+				Intent nextScreen = new Intent(SummaryPage.this.getContext(), AllData.class);
+				startActivity(nextScreen);
 			}
 		});
 	}
--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/SummaryPageAdapter.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/SummaryPageAdapter.java
@@ -1,9 +1,11 @@
 package edu.ua.cs.cs495.caladrius.android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import org.json.JSONException;
 
 import java.io.IOException;
@@ -14,16 +16,20 @@ import java.util.concurrent.ExecutionException;
  * The FitBitGraphViewAdapter module generates FitbitGraphView objects based on predetermined
  * lists of input parameters.
  */
-public class FitbitGraphViewAdapter extends BaseAdapter
+public class SummaryPageAdapter extends BaseAdapter
 {
 	public interface FitbitGraphViewOnClickAdapter {
 		void onClick(FitbitGraphView fgv);
 	}
 
 	ArrayList<FitbitGraphView> fgvs;
-	public FitbitGraphViewAdapter(final Context cntxt,
-								  final ArrayList<Query> queries, final FitbitGraphViewOnClickAdapter onClick)  throws JSONException, InterruptedException, ExecutionException, IOException
+	View lastElement;
+	public SummaryPageAdapter(final Context cntxt, final ArrayList<Query> queries,
+							  final FitbitGraphViewOnClickAdapter onClick, final View lastElement)
+			throws JSONException, InterruptedException, ExecutionException, IOException
 	{
+		this.lastElement = lastElement;
+
 	    fgvs = new ArrayList();
 	    for (int x = 0; x < queries.size(); x++) {
 	    	FitbitGraphView fgv = new FitbitGraphView(cntxt, queries.get(x));
@@ -46,13 +52,19 @@ public class FitbitGraphViewAdapter extends BaseAdapter
 	@Override
 	public int getCount()
 	{
-		return fgvs.size();
+		return fgvs.size() + 1;
 	}
 
 	@Override
 	public Object getItem(int i)
 	{
-		return fgvs.get(i);
+		if (i < fgvs.size()) {
+			assert(i >= 0);
+			return fgvs.get(i);
+		} else {
+			assert(i == fgvs.size());
+			return lastElement;
+		}
 	}
 
 	@Override
@@ -64,6 +76,12 @@ public class FitbitGraphViewAdapter extends BaseAdapter
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup)
 	{
-		return fgvs.get(i);
+		if (i < fgvs.size()) {
+			assert(i >= 0);
+			return fgvs.get(i);
+		} else {
+			assert(i == fgvs.size());
+			return lastElement;
+		}
 	}
 }
