278fc11f340e54647fa10880c9bfcf514ec89af3
Ivan Johnson <git@IvanJohnson.net>
2018-11-24 17:54:30 -0600

    Implement client side interface to server

---
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/server/Clientside.java
@@ -0,0 +1,184 @@
+package edu.ua.cs.cs495.caladrius.server;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Base64;
+import java.util.Random;
+import java.util.Scanner;
+
+import edu.ua.cs.cs495.caladrius.rss.Feed;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Clientside
+{
+	public OkHttpClient client = new OkHttpClient();
+
+	public int[] getFeedIDs(String userid) throws IOException
+	{
+		final String URL = "https://caladrius.ivanjohnson.net/webapi/config/feeds";
+
+		Request request = new Request.Builder()
+			.url(URL)
+			.addHeader("useruuid", userid)
+			.build();
+
+		Response r = client.newCall(request).execute();
+
+		byte[] bytes = r.body().bytes();
+
+		ByteArrayInputStream bais;
+		Scanner s;
+
+		bais = new ByteArrayInputStream(bytes);
+		s = new Scanner(bais, "UTF-8");
+		int numLines = 0;
+		while(s.hasNextLine()) {
+			numLines++;
+			s.nextLine();
+		}
+		s.close();
+
+		int feedids[] = new int[numLines];
+		bais = new ByteArrayInputStream(bytes);
+		s = new Scanner(bais, "UTF-8");
+		while(s.hasNextLine()) {
+			numLines--;
+			feedids[numLines] = Integer.parseInt(s.nextLine());
+		}
+		s.close();
+
+		return feedids;
+	}
+
+	protected String getFeedstring(String userid, int feed)
+	{
+		final String URL_BASE = "https://caladrius.ivanjohnson.net/webapi/config/feed";
+		final String URL_QUERY_KEY = "id";
+
+		final String url = URL_BASE + "?" + URL_QUERY_KEY + "=" + feed;
+
+		long lNow = System.currentTimeMillis() / 1000L;
+
+		Request request = new Request.Builder()
+			.url(url)
+			.addHeader("useruuid", userid)
+			.addHeader("MODDATE", Long.toString(lNow))
+			.build();
+
+		Scanner s = null;
+		try {
+			Response r = client.newCall(request)
+			                   .execute();
+			if (r.code() != 200) {
+				return null;
+			}
+
+			s = new Scanner(r.body().byteStream(), "UTF-8");
+			s.useDelimiter("\\A");
+		} catch (IOException e) {
+			return null;
+		}
+
+		String str = s.hasNext() ? s.next() : ""; // base64: ..., lastModify: ...
+		String attribute = str.split(",")[0];     // base64: ...
+		String base64 = attribute.split(" ")[1];  // ...
+
+		s.close();
+		return base64;
+	}
+
+	public void setFeed(String userid, int feed, String base64) throws IOException
+	{
+		final String URL_BASE = "https://caladrius.ivanjohnson.net/webapi/config/feed";
+		final String URL_QUERY_KEY = "id";
+		final String url = URL_BASE + "?" + URL_QUERY_KEY + "=" + feed;
+
+		long lNow = System.currentTimeMillis() / 1000L;
+
+		if (base64.length() > 100000) {
+			throw new IllegalArgumentException("Given string is " + base64.length() + " char long; it must be less than 100000 to fit in the database.");
+		}
+
+		MediaType mt = MediaType.parse("text/plain; charset=utf-8");
+
+		RequestBody body = RequestBody.create(mt, base64.getBytes("UTF-8"));
+
+		Request request = new Request.Builder()
+			.url(url)
+			.put(body)
+			.addHeader("useruuid", userid)
+			.addHeader("MODDATE", Long.toString(lNow))
+			.build();
+
+		Response r = client.newCall(request)
+		                   .execute();
+		if (r.code() != 200) {
+			throw new IOException("Server responded with code " + r.code());
+		}
+	}
+
+	public Feed getFeed(String userid, int feed) throws IOException
+	{
+		String base64 = getFeedstring(userid, feed);
+		byte bytes[] = Base64.getDecoder().decode(base64);
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		ObjectInputStream ois = null;
+		try {
+			ois = new ObjectInputStream(bis);
+			Feed f = (Feed) ois.readObject();
+			return f;
+		} catch (ClassNotFoundException cnfe) {
+			return null;
+		} finally {
+			if (ois != null) {
+				ois.close();
+			}
+		}
+	}
+
+	public void setFeed(String userid, Feed f) throws IOException
+	{
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(bos);
+			out.writeObject(f);
+			out.flush();
+			byte[] bytes = bos.toByteArray();
+			String base64 = Base64.getEncoder().encodeToString(bytes);
+			setFeed(userid, f.id, base64);
+		} finally {
+			try {
+				bos.close();
+			} catch (IOException ex) {
+				// NOP
+			}
+		}
+	}
+
+	public static void main(String args[]) throws IOException
+	{
+		final String UUID = "thisisauserid1";
+		Clientside cs = new Clientside();
+		int ids[] = cs.getFeedIDs(UUID);
+		Random r = new Random();
+		for (int id : ids) {
+			System.out.println(id);
+			Feed fPush = new Feed("name #"+r.nextInt(), "URL #"+r.nextInt());
+			fPush.id = id;
+			cs.setFeed(UUID, fPush);
+
+			Feed fPull = cs.getFeed(UUID, id);
+
+			System.out.println(fPush.toString());
+			System.out.println(fPull.toString());
+		}
+	}
+}
