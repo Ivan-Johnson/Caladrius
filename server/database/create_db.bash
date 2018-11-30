a2972edac136da7e058702b17932c41e8278eab5
Ivan Johnson <git@IvanJohnson.net>
2018-11-30 12:08:38 -0600

    Prep database for tests

--- server/database/create_db.bash
+++ server/database/create_db.bash
@@ -6,8 +6,33 @@ fi
 
 DB_FILE="/srv/caladrius.db"
 
+trash "$DB_FILE"
+
+###############
+#CREATE TABLES#
+###############
 sqlite3 "$DB_FILE" 'CREATE TABLE feeds       ( userid VARCHAR(39), feedid VARCHAR(39), feedbase64 VARCHAR(100000), lastFetch BIGINT, lastModify BIGINT,  PRIMARY KEY (userid, feedid));'
-#sqlite3 "$DB_FILE" "INSERT INTO  feeds values('thisisauserid1',   '1001',             'lololol5',                0,                0);"
-#sqlite3 "$DB_FILE" "INSERT INTO  feeds values('thisisauserid1',   '1002',             'lololol6',                0,                0);"
-#sqlite3 "$DB_FILE" "INSERT INTO  feeds values('thisisauserid2',   '1003',             'lololol7',                0,                0);"
-#sqlite3 "$DB_FILE" "INSERT INTO  feeds values('thisisauserid2',   '1004',             'lololol8',                0,                0);"
+
+
+###########
+#TEST DATA#
+###########
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_0',   '11573',             'DEADBEEF',                0,                0);"
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_0',   '12484',             'DEADBEEF',                0,                0);"
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_0',   '18693',             'DEADBEEF',                0,                0);"
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_0',   '4144',              'DEADBEEF',                0,                0);"
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_0',   '26184',             'DEADBEEF',                0,                0);"
+
+# TEST_USER_1 has no feeds
+
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_2',   '6427',              'DEADBEEF',                0,                0);"
+
+
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_3',   '27560',              '1XDSpDji2jcqJoQgfQpA8zC6rnK7uJ31OPObAccMXKEo6CLuVSYTUcNer85r9Vvo3Z1PE6EoxPgjUODEsWqSSMUZ2tfUnbrPyWMJkc43ZsfNNS4RMgz4tYXbeQ7cccDS',                0,                0);"
+
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_4',   '26814',              '',                0,                0);"
+
+
+sqlite3 "$DB_FILE" "INSERT INTO  feeds values('TEST_USER_5',   '10194',              '',                0,                0);"
+
+# TEST_USER_6 is empty for the purpose of identity testing; i.e. set then immediately get
