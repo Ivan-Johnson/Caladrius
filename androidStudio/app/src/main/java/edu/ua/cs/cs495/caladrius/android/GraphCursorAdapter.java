a17f6a1aebccac30572b3f5bcd2dc8384b9e45fa
Ivan Johnson <git@IvanJohnson.net>
2018-12-05 15:45:58 -0600

    Fix formatting in GraphCursorAdapter

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/GraphCursorAdapter.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/GraphCursorAdapter.java
@@ -30,141 +30,153 @@ import static edu.ua.cs.cs495.caladrius.android.Caladrius.getContext;
  *
  * @author Hansheng Li
  */
-public class GraphCursorAdapter extends CursorAdapter {
+public class GraphCursorAdapter extends CursorAdapter
+{
 
-    private Integer mPosition;
+	private Integer mPosition;
 
-    GraphCursorAdapter(Context context, Cursor c, Integer p) {
-        super(context, c, 0 /* flags */);
-        mPosition = p;
-    }
+	GraphCursorAdapter(Context context, Cursor c, Integer p)
+	{
+		super(context, c, 0 /* flags */);
+		mPosition = p;
+	}
 
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.graph_list_item, parent, false);
-    }
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent)
+	{
+		return LayoutInflater.from(context)
+		                     .inflate(R.layout.graph_list_item, parent, false);
+	}
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+	@Override
+	public void bindView(View view, Context context, Cursor cursor)
+	{
 
-        // Find the columns of graph attributes that we're interested in
-        int timeRangeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TIME_RANGE);
-        int typeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TYPE);
-        int statsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_STATS);
-        int colorColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_COLORS);
-        int titleColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TITLE);
-        int graph2TypeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_TYPE);
-        int graph2StatsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_STATS);
-        int graph2ColorColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_COLORS);
-        int numberGraphsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_NUMBER_OF_GRAPH);
-        int timeRangeTypeGraphsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TIME_RANGE_TYPE);
-        int startTimeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_START_TIME);
-        int endTimeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_END_TIME);
+		// Find the columns of graph attributes that we're interested in
+		int timeRangeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TIME_RANGE);
+		int typeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TYPE);
+		int statsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_STATS);
+		int colorColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_COLORS);
+		int titleColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TITLE);
+		int graph2TypeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_TYPE);
+		int graph2StatsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_STATS);
+		int graph2ColorColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH2_COLORS);
+		int numberGraphsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_NUMBER_OF_GRAPH);
+		int timeRangeTypeGraphsColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_TIME_RANGE_TYPE);
+		int startTimeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_START_TIME);
+		int endTimeColumnIndex = cursor.getColumnIndex(GraphEntry.COLUMN_GRAPH_END_TIME);
 
-        int idColumnIndex = cursor.getColumnIndex(GraphEntry._ID);
+		int idColumnIndex = cursor.getColumnIndex(GraphEntry._ID);
 
-        // Read the graph attributes from the Cursor for the current graph
-        String timeRangeTypeGraphs = cursor.getString(timeRangeTypeGraphsColumnIndex);
-        String startTime = cursor.getString(startTimeColumnIndex);
-        String endTime = cursor.getString(endTimeColumnIndex);
-        String graphTimeRange = cursor.getString(timeRangeColumnIndex);
-        final String graphType = cursor.getString(typeColumnIndex);
-        final String graphStats = cursor.getString(statsColumnIndex);
-        final String graphColor = cursor.getString(colorColumnIndex);
-        final String graphTitle = cursor.getString(titleColumnIndex);
-        final String graphId = cursor.getString(idColumnIndex);
-        final String graph2Type = cursor.getString(graph2TypeColumnIndex);
-        final String graph2Stats = cursor.getString(graph2StatsColumnIndex);
-        final String graph2Color = cursor.getString(graph2ColorColumnIndex);
-        final String numberOfGraph = cursor.getString(numberGraphsColumnIndex);
-
-
-        List<String> timeRangeList = Arrays.asList(context.getResources().getStringArray(R.array.array_time_range_options));
-
-        List<String> typeList = Arrays.asList(context.getResources().getStringArray(R.array.array_graph_type_options));
-
-
-        final List<String> statsList = Arrays.asList(context.getResources().getStringArray(R.array.array_graph_stats_options));
+		// Read the graph attributes from the Cursor for the current graph
+		String timeRangeTypeGraphs = cursor.getString(timeRangeTypeGraphsColumnIndex);
+		String startTime = cursor.getString(startTimeColumnIndex);
+		String endTime = cursor.getString(endTimeColumnIndex);
+		String graphTimeRange = cursor.getString(timeRangeColumnIndex);
+		final String graphType = cursor.getString(typeColumnIndex);
+		final String graphStats = cursor.getString(statsColumnIndex);
+		final String graphColor = cursor.getString(colorColumnIndex);
+		final String graphTitle = cursor.getString(titleColumnIndex);
+		final String graphId = cursor.getString(idColumnIndex);
+		final String graph2Type = cursor.getString(graph2TypeColumnIndex);
+		final String graph2Stats = cursor.getString(graph2StatsColumnIndex);
+		final String graph2Color = cursor.getString(graph2ColorColumnIndex);
+		final String numberOfGraph = cursor.getString(numberGraphsColumnIndex);
 
 
-        List<String> colorList = Arrays.asList(context.getResources().getStringArray(R.array.array_graph_color_options));
+		List<String> timeRangeList = Arrays.asList(context.getResources()
+		                                                  .getStringArray(R.array.array_time_range_options));
 
-        LinearLayout graph_container = view.findViewById(R.id.graph_container);
-
-        ArrayList<FitbitGraphView.GraphViewGraph> graphTypes =
-                new ArrayList<FitbitGraphView.GraphViewGraph>(){{
-                    add(GraphEntry.getGraphType(Integer.valueOf(graphType)));
-        }};
-
-        ArrayList<String> stats = new ArrayList<String>(){{
-            add(statsList.get(Integer.valueOf(graphStats)));
-        }};
+		List<String> typeList = Arrays.asList(context.getResources()
+		                                             .getStringArray(R.array.array_graph_type_options));
 
 
-        ArrayList<Integer> color = new ArrayList<Integer>(){{
-            add(GraphEntry.GetColour(Integer.valueOf(graphColor)));
-        }};
-
-        if(Integer.valueOf(numberOfGraph) == GraphEntry.GRAPH_NUMBER_TWO){
-            stats.add(statsList.get(Integer.valueOf(graph2Stats)));
-            color.add(GraphEntry.GetColour(Integer.valueOf(graph2Color)));
-            graphTypes.add(GraphEntry.getGraphType(Integer.valueOf(graph2Type)));
-        }
-
-        Query query = new Query(graphTypes,
-                                stats,
-                                color,
-                                graphTitle,
-                                startTime,
-                                endTime,
-                                Integer.valueOf(timeRangeTypeGraphs),
-                                Integer.valueOf(graphTimeRange));
+		final List<String> statsList = Arrays.asList(context.getResources()
+		                                                    .getStringArray(R.array.array_graph_stats_options));
 
 
-        FitbitGraphView fgv = null;
-        try {
-            fgv = new FitbitGraphView(getContext(), query);
-            if (mPosition == 0){
-                fgv.setOnClickListener(view1 ->
-                {
-                    Intent intent = new Intent(getContext(), GraphEditorActivity.class);
+		List<String> colorList = Arrays.asList(context.getResources()
+		                                              .getStringArray(R.array.array_graph_color_options));
 
-                    Uri currentPetUri = ContentUris.withAppendedId(
-                            GraphEntry.CONTENT_URI, Integer.valueOf(graphId));
+		LinearLayout graph_container = view.findViewById(R.id.graph_container);
 
-                    intent.setData(currentPetUri);
+		ArrayList<FitbitGraphView.GraphViewGraph> graphTypes =
+			new ArrayList<FitbitGraphView.GraphViewGraph>()
+			{{
+				add(GraphEntry.getGraphType(Integer.valueOf(graphType)));
+			}};
 
-                    getContext().startActivity(intent);
-                });
-            } else {
-                fgv.setOnClickListener(view1 ->
-                {
-                    Intent intent = new Intent(getContext(), QueryActivity.class);
-                    intent.putExtra("startDate", startTime);
-                    intent.putExtra("endDate", endTime);
-                    intent.putExtra("graph_1_stats", Integer.valueOf(graphStats));
-                    intent.putExtra("graph_2_stats", Integer.valueOf(graph2Stats));
-                    intent.putExtra("num_graph", Integer.valueOf(numberOfGraph));
-                    intent.putExtra("graph_1_color", GraphEntry.GetColour(Integer.valueOf(graphColor)));
-                    intent.putExtra("graph_2_color", GraphEntry.GetColour(Integer.valueOf(graph2Color)));
-                    intent.putExtra("graph_1_type", Integer.valueOf(graphType));
-                    intent.putExtra("graph_2_type", Integer.valueOf(graph2Type));
-                    intent.putExtra("time_range_type", Integer.valueOf(timeRangeTypeGraphs));
-                    intent.putExtra("relative_time_type", Integer.valueOf(graphTimeRange));
-                    intent.putExtra("graph_title", graphTitle);
-                    getContext().startActivity(intent);
-                });
-
-            }
-        } catch (JSONException | InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
-        }
-
-        if((graph_container).getChildCount() > 0)
-            (graph_container).removeAllViews();
-
-        graph_container.addView(fgv);
+		ArrayList<String> stats = new ArrayList<String>()
+		{{
+			add(statsList.get(Integer.valueOf(graphStats)));
+		}};
 
 
-    }
+		ArrayList<Integer> color = new ArrayList<Integer>()
+		{{
+			add(GraphEntry.GetColour(Integer.valueOf(graphColor)));
+		}};
+
+		if (Integer.valueOf(numberOfGraph) == GraphEntry.GRAPH_NUMBER_TWO) {
+			stats.add(statsList.get(Integer.valueOf(graph2Stats)));
+			color.add(GraphEntry.GetColour(Integer.valueOf(graph2Color)));
+			graphTypes.add(GraphEntry.getGraphType(Integer.valueOf(graph2Type)));
+		}
+
+		Query query = new Query(graphTypes,
+			stats,
+			color,
+			graphTitle,
+			startTime,
+			endTime,
+			Integer.valueOf(timeRangeTypeGraphs),
+			Integer.valueOf(graphTimeRange));
+
+
+		FitbitGraphView fgv = null;
+		try {
+			fgv = new FitbitGraphView(getContext(), query);
+			if (mPosition == 0) {
+				fgv.setOnClickListener(view1 ->
+				{
+					Intent intent = new Intent(getContext(), GraphEditorActivity.class);
+
+					Uri currentPetUri = ContentUris.withAppendedId(
+						GraphEntry.CONTENT_URI, Integer.valueOf(graphId));
+
+					intent.setData(currentPetUri);
+
+					getContext().startActivity(intent);
+				});
+			} else {
+				fgv.setOnClickListener(view1 ->
+				{
+					Intent intent = new Intent(getContext(), QueryActivity.class);
+					intent.putExtra("startDate", startTime);
+					intent.putExtra("endDate", endTime);
+					intent.putExtra("graph_1_stats", Integer.valueOf(graphStats));
+					intent.putExtra("graph_2_stats", Integer.valueOf(graph2Stats));
+					intent.putExtra("num_graph", Integer.valueOf(numberOfGraph));
+					intent.putExtra("graph_1_color",
+						GraphEntry.GetColour(Integer.valueOf(graphColor)));
+					intent.putExtra("graph_2_color",
+						GraphEntry.GetColour(Integer.valueOf(graph2Color)));
+					intent.putExtra("graph_1_type", Integer.valueOf(graphType));
+					intent.putExtra("graph_2_type", Integer.valueOf(graph2Type));
+					intent.putExtra("time_range_type", Integer.valueOf(timeRangeTypeGraphs));
+					intent.putExtra("relative_time_type", Integer.valueOf(graphTimeRange));
+					intent.putExtra("graph_title", graphTitle);
+					getContext().startActivity(intent);
+				});
+			}
+		} catch (JSONException | InterruptedException | ExecutionException | IOException e) {
+			e.printStackTrace();
+		}
+
+		if ((graph_container).getChildCount() > 0) {
+			(graph_container).removeAllViews();
+		}
+
+		graph_container.addView(fgv);
+	}
 }
