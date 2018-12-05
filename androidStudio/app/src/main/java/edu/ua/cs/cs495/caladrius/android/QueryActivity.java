1563edc5c89248457b0e3ce7998ce96549cea361
Peter Jackson <43251604+PeebChochman@users.noreply.github.com>
2018-12-05 15:39:36 -0600

    added aligned tables to queryactivity

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/QueryActivity.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/android/QueryActivity.java
@@ -2,11 +2,16 @@ package edu.ua.cs.cs495.caladrius.android;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.*;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -119,15 +124,19 @@ public class QueryActivity extends AppCompatActivity {
 		cardView.addView(fgv);
 		final List<String> statsListPretty = Arrays.asList(this.getResources().getStringArray(R.array.array_graph_stats_options_pretty));
 
+		String stat1_header = "";
+		String stat2_header = "";
 		ArrayList<DataPair> points = new ArrayList<>();
 		try {
 			JSONArray arr1 = Caladrius.fitbitInterface.getFitbitData(statsList.get(graphStats), timeRangeTypeGraphs, startTime, endTime, graphTimeRange);
 			JSONArray arr2 = new JSONArray();
 			final TextView headerValue1 = findViewById(R.id.header_value);
 			headerValue1.setText(statsListPretty.get(graphStats));
+			stat1_header = statsListPretty.get(graphStats);
 			if(numberOfGraph == GraphContract.GraphEntry.GRAPH_NUMBER_TWO) {
 				final TextView headerValue2 = findViewById(R.id.header_value2);
 				headerValue2.setText(statsListPretty.get(graph2Stats));
+				stat2_header = statsListPretty.get(graph2Stats);
 				arr2 = Caladrius.fitbitInterface.getFitbitData(statsList.get(graph2Stats), timeRangeTypeGraphs, startTime, endTime, graphTimeRange);
                 for (int x = 0; x < arr2.length(); x++) {
                     String dt = arr2.getJSONObject(x).getString("dateTime");
@@ -153,11 +162,61 @@ public class QueryActivity extends AppCompatActivity {
 		}
 		Collections.sort(points, new SortbyDate());
 
-		initializeRecyclerView();
-		populateRecyclerView(points);
+		//initializeRecyclerView();
+		//populateRecyclerView(points);
 
-		final TextView headerDate = findViewById(R.id.header_date);
-		headerDate.setText("Date");
+		//final TextView headerDate = findViewById(R.id.header_date);
+		//headerDate.setText("Date");
+
+
+
+		TableRow.LayoutParams wrapWrapTableRowParams = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		int[] fixedColumnWidths = new int[]{26, 34, 34};
+		int[] scrollableColumnWidths = new int[]{26, 34, 34};
+		int fixedRowHeight = 70;
+		int fixedHeaderHeight = 80;
+
+		TableRow row = new TableRow(this);
+		//header (fixed vertically)
+		TableLayout header = (TableLayout) findViewById(R.id.table_header);
+		row.setLayoutParams(wrapWrapTableRowParams);
+		row.setBackgroundColor(Color.GRAY);
+		row.addView(makeTableRowWithText("Date", fixedColumnWidths[0], fixedHeaderHeight));
+		row.addView(makeTableRowWithText(stat1_header, fixedColumnWidths[1], fixedHeaderHeight));
+		row.addView(makeTableRowWithText(stat2_header, fixedColumnWidths[2], fixedHeaderHeight));
+		header.addView(row);
+		//header (fixed horizontally)
+		TableLayout fixedColumn = (TableLayout) findViewById(R.id.fixed_column);
+		//rest of the table (within a scroll view)
+		TableLayout scrollablePart = (TableLayout) findViewById(R.id.scrollable_part);
+		for(int i = 0; i < points.size(); i++) {
+			TextView fixedView = makeTableRowWithText(points.get(i).getDate(), scrollableColumnWidths[0], fixedRowHeight);
+			fixedView.setBackgroundColor(Color.LTGRAY);
+			fixedColumn.addView(fixedView);
+			row = new TableRow(this);
+			row.setLayoutParams(wrapWrapTableRowParams);
+			row.setGravity(Gravity.CENTER);
+			row.addView(makeTableRowWithText(points.get(i).getValue(), scrollableColumnWidths[1], fixedRowHeight));
+			if (!points.get(i).getValue2().equals("-1"))
+				row.addView(makeTableRowWithText(points.get(i).getValue2(), scrollableColumnWidths[2], fixedRowHeight));
+			scrollablePart.addView(row);
+		}
+	}
+
+
+
+	//util method
+	private TextView recyclableTextView;
+
+	public TextView makeTableRowWithText(String text, int widthInPercentOfScreenWidth, int fixedHeightInPixels) {
+		int screenWidth = getResources().getDisplayMetrics().widthPixels;
+		recyclableTextView = new TextView(this);
+		recyclableTextView.setText(text);
+		recyclableTextView.setTextColor(Color.BLACK);
+		recyclableTextView.setTextSize(20);
+		recyclableTextView.setWidth(widthInPercentOfScreenWidth * screenWidth / 100);
+		recyclableTextView.setHeight(fixedHeightInPixels);
+		return recyclableTextView;
 	}
 
 	private void initializeRecyclerView() {
