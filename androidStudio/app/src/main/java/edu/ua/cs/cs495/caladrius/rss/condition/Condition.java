cd9714425b4a8b6170145625cb041e4adf7cb125
Ivan Johnson <git@IvanJohnson.net>
2018-12-06 11:55:38 -0600

    Document Condition

--- androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/rss/condition/Condition.java
+++ androidStudio/app/src/main/java/edu/ua/cs/cs495/caladrius/rss/condition/Condition.java
@@ -5,9 +5,20 @@ import org.json.JSONArray;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * A condition stores the situations underwhich an RSS feed should have items added to it.
+ */
 public interface Condition extends Serializable
 {
-	public ArrayList<String> getMatches(JSONArray data);
+	/**
+	 * @param data a json array where each element is a single day of the stat specified by getStat. Each element
+	 * 	has two values; "value" and "dateTime"
+	 * @return an arraylist of strings to be added to the RSS feed
+	 */
+	ArrayList<String> getMatches(JSONArray data);
 
-	public String getStat();
+	/**
+	 * @return the name of the stat whose data should be passed to getMatches
+	 */
+	String getStat();
 }
